2b9db0369d334617924ee5373927f54a43595465
Gutierrez Martinez, Adrian Aaron <Adrian.Gutierrez-Martinez@gft.com>
2016-08-30 16:57:39 +0200

    +clear focus on text fixed +labels on views added +date picker initial date added +nativescript angular rc4 stablished

--- app/pages/text/text.component.ts
+++ app/pages/text/text.component.ts
@@ -1,9 +1,9 @@
-import {Component} from "@angular/core";
-import {ContentService} from "../../common/services/content.service";
-import {Page} from "ui/page";
+import {Component} from '@angular/core';
+import {ContentService} from '../../common/services/content.service';
+import {Page} from 'ui/page';
 
 @Component({
-    selector: "TextPage",
+    selector: 'TextPage',
     templateUrl: 'pages/text/text.html',
     styleUrls: ['pages/text/text.css']
 })
@@ -11,33 +11,38 @@ export class TextPage {
 
     private styleSelected: number = 0;
     private colorSelected: number = 0;
-    private style: string = "light";
+    private style: string = 'light';
     private textStyles: any[];
     private styles: any[];
     private colors: any[];
     private opening: string;
     private rounded: boolean = false;
 
-    public constructor(private _contentService: ContentService, private page: Page){
+    public constructor(private _contentService: ContentService, private page: Page) {
         this.colors = _contentService.getColors();
         this.opening = _contentService.getOpening();
         this.styles = _contentService.getStyles();
         this.textStyles = _contentService.getTextStyles();
     }
 
-    public textFieldLoaded(args){
-        args.object.dismissSoftInput()
+    public textFieldLoaded(args) {
+        args.object.dismissSoftInput();
+        if (args.object.ios) {
+        args.object.ios.endEditing(true);
+        } else if (args.object.android) {
+        args.object.android.clearFocus();
+        }
     }
-    
-    public switchChange(args){
+
+    public switchChange(args) {
         this.rounded = !this.rounded;
     }
 
-    public selectedIndexChanged(args){
+    public selectedIndexChanged(args) {
         this.styleSelected = args.object.selectedIndex;
     }
 
-    public selectedColorChanged(args){
+    public selectedColorChanged(args) {
         this.colorSelected = args.object.selectedIndex;
     }
 }
